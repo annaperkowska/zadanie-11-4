66bf1b7e95f96817635292daa4f1f0f5ffa10878
Anna Perkowska <perkowska.a@gmail.com>
2018-02-15 10:18:47 +0100

    Change script.js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ Phone.prototype.printInfo = function() {
 	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is $" + this.price)
 }
 
+Phone.prototype.alertInfo = function() {
+	alert("Final sale! Only today " + this.brand + " (" + this.color + ") with discount: -25%!")
+}
+
 var iPhone6S = new Phone("Apple", 5555, "red"),
 	GalaxyS6 = new Phone("Samsung", 4444, "yellow"),
 	One = new Phone("OnePlus", 3333, "green"),
@@ -20,3 +24,5 @@ GalaxyS6.printInfo();
 One.printInfo();
 
 Nokia3310.printInfo();
+
+Nokia3310.alertInfo();
